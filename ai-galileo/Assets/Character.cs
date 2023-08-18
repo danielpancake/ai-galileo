528ad15d5aede966c9ceb9cc470d09408ed4e796
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-08-18 21:37:27 +0500

    Added animations and character

---
+++ ai-galileo/Assets/Character.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+enum AnimationState
+{
+    Fall,
+    Idle,
+    Jump,
+    Run,
+    Sprint,
+    Strafe,
+    Talk,
+    Walk,
+}
+
+public class Character : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+    private AnimatorController m_animatorController;
+
+    private AnimationState m_animationState = AnimationState.Idle;
+
+    public float m_speed = 5f;
+    public float m_jumpAmount = 5f;
+
+    private float lastYPosition;
+
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_animatorController = GetComponent<AnimatorController>();
+
+        lastYPosition = transform.position.y;
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
+    }
+
+    void Update()
+    {
+        Vector3 _v = new(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        m_rigidbody.MovePosition(transform.position + _v * Time.deltaTime * m_speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            m_rigidbody.AddForce(Vector3.up * m_jumpAmount, ForceMode.Impulse);
+        }
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_v), Time.deltaTime * 40f);
+    }
+}
