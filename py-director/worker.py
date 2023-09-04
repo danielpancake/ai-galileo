df501ddb8b93327d449d6aee96d77966ebaa7c7d
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-09-04 14:10:23 +0300

    Added proper worker for each platform

--- py-director/worker.py
+++ py-director/worker.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 
-from rq import Connection
+from rq import Connection, SimpleWorker
 from rq_win import WindowsWorker
 
+import platform
 import sys
 
 with Connection():
@@ -10,5 +11,6 @@ with Connection():
 
     load_dotenv()
 
-    w = WindowsWorker(qs)
+    # Use WindowsWorker if on Windows, otherwise use SimpleWorker
+    w = WindowsWorker(qs) if platform.system() == "Windows" else SimpleWorker(qs)
     w.work()
