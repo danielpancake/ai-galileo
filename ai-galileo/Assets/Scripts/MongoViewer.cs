43ce4757234496d5d249bcc9ba80759a3979251d
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-09-17 17:45:19 +0300

    Added sequential audio playback

--- ai-galileo/Assets/Scripts/MongoViewer.cs
+++ ai-galileo/Assets/Scripts/MongoViewer.cs
@@ -29,32 +29,107 @@ public class MongoViewer : MonoBehaviour
     private MongoClient client;
     private IMongoDatabase db;
 
+    private List<Model_Story> stories;
+
+    private AudioSource audioSource;
+
+    private int current_story = 0;
+    private int current_node = 0;
+
+
     void Start()
     {
         client = new MongoClient(MONGO_URI);
         db = client.GetDatabase(MONGO_DATABASE);
 
-        // Get all stories
-        List<Model_Story> stories = getStories();
-        foreach (Model_Story story in stories)
-        {
-            for (int i = 0; i < story.story.Count; i++)
-            {
-                Model_StoryNode node = story.story[i];
-                Debug.Log(node.type + " " + node.voice + " " + node.action + " " + node.text);
-            }
-        }
+        stories = getStories();
+
+        PlayNextClip();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-
+        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     List<Model_Story> getStories()
     {
         // Retrieve all stories
         return db.GetCollection<Model_Story>("stories").Find(_ => true).ToList();
+    }
+
+    void PlayNextClip()
+    {
+        Model_Story story = stories[current_story];
+        Model_StoryNode node = story.pre_story[current_node];
+
+        bool found = false;
+
+        while (!found)
+        {
+            switch (node.type)
+            {
+                case "text":
+                    found = true;
+                    break;
+                default:
+                    IncementNode();
+                    node = story.pre_story[current_node];
+                    break;
+            }
+        }
+
+        switch (node.type)
+        {
+            case "text":
+                Debug.Log(node.text);
+                if (node.voice != null)
+                {
+                    WWW www = GetAudioFromFile(node.voice);
+                    StartCoroutine(LoadAndPlay(www));
+                }
+                break;
+        }
+    }
+
+    private WWW GetAudioFromFile(string filepath)
+    {
+        WWW request = new("file://" + filepath);
+        return request;
+    }
+
+    private void IncementNode()
+    {
+        current_node += 1;
+
+        // Check if we are at the end of the story
+        if (current_node >= stories[current_story].pre_story.Count)
+        {
+            current_story += 1;
+            current_node = 0;
+        }
+
+        // Check if we are at the end of the stories
+        if (current_story >= stories.Count)
+        {
+            current_story = 0;
+            current_node = 0;
+        }
+    }
+
+    IEnumerator LoadAndPlay(WWW www)
+    {
+        yield return www;
+
+        audioSource.clip = www.GetAudioClip();
+        audioSource.Play();
+
+        while (GetComponent<AudioSource>().isPlaying)
+        {
+            yield return null;
+        }
+
+        IncementNode();
+        PlayNextClip();
     }
 }
